3c6eb9cacff77c36a9bb175f4da32f7f2ec980eb
mezno <marcel.therence@gmail.com>
2018-03-09 21:56:01 +0100

    Ajout des méthodes seDeplacer(),boire() et manger()

--- Personne.java
+++ Personne.java
@@ -25,6 +25,22 @@ public Personne(String nom,String prenom,int age){
 }
 
 
+public void seDeplacer(){
+
+	System.out.println("Je me déplace à vélo");
+}
+
+
+public void boire(){
+
+	System.out.println("Je bois de l'eau");
+}
+
+public void manger(){
+
+	System.out.println("Je mange du riz");
+}
+
 
 
 
