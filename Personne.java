463b0c550462e1331d6b6359a2755c28d45bb444
mezno <37047553+mezno@users.noreply.github.com>
2018-03-09 21:24:11 +0100

    premiere version de la classe Personne

---
+++ Personne.java
@@ -0,0 +1,22 @@
+
+public class Personne {
+
+
+public String nom;
+public String prenom;
+public int age;
+
+
+public Personne(){
+this.nom="inconnu";
+this.prenom="inconnu";
+this.age=0;
+
+}
+
+
+
+
+
+
+}
